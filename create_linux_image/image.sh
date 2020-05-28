066bab438f3e5d80bd9b7fb98135fe6b1042232a
Conrad <conrad.corbett@nutanix.com>
2020-05-28 12:47:32 -0700

    not workng, returning "acli: command not found" when run in atom using Script Runner

---
+++ create_linux_image/image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Printing text with newline"
+
+#ssh to CVM and run command
+ssh nutanix@10.38.15.134 "acli image.create linux2 source_url=http://10.42.194.11/images/AHV/Centos7.qcow2 image_type=kDiskImage container=Images"
